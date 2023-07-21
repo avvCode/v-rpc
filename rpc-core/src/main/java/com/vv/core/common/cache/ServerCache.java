68d86af5eb2678f805b69466495454c0f5f61714
zyz19 <80141182@qq.com>
2023-07-21 17:43:34 +0800

    服务端新增 服务存储

---
+++ rpc-core/src/main/java/com/vv/core/common/cache/ServerCache.java
@@ -0,0 +1,13 @@
+package com.vv.core.common.cache;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author vv
+ * @Description 存储服务
+ * @date 2023/7/21-13:29
+ */
+public class ServerCache {
+    public static final Map<String,Object> PROVIDER_CLASS_MAP = new HashMap<>();
+}
